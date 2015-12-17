39a8dc0926c37309dd6319d81c08e84402c31c86
Samuel Baptista <samuel.baptista@ecodev.ch>
2015-12-17 19:14:29 +0100

    Add basic carousel #3847

--- Resources/Public/JavaScript/CarouselGallery.js
+++ Resources/Public/JavaScript/CarouselGallery.js
@@ -4,8 +4,50 @@
 (function($) {
     $(function() {
 
-
         function initGallery() {
+
+            for (var i = 0; i < carouselGalleries.length; i++) {
+
+                var gallery = carouselGalleries[i];
+
+                gallery.selector = '#tx-carouselgallery-' + gallery.id;
+                gallery.rootElement = $(gallery.selector);
+                gallery.bodyElement = gallery.rootElement.find('.swiper-wrapper');
+
+                for (var j = 0; j < gallery.images.length; j++) {
+                    var slide = $('<div class="swiper-slide swiper-lazy"></div>')
+                        .attr('data-background', gallery.images[j].enlarged)
+                        .append('<div class="swiper-lazy-preloader"></div>');
+
+                    gallery.bodyElement.append(slide);
+                }
+
+                gallery.swiper = new Swiper(gallery.rootElement.selector, {
+                    pagination: '.swiper-pagination',
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+
+                    loop: gallery.loop,
+                    speed: gallery.transitionTime,
+                    autoplay: gallery.waitTime, // @todo : implement gallery.autoplay setting
+                    autoplayDisableOnInteraction: gallery.autoReplay,
+                    slidesPerView: gallery.slidesPerView,
+
+                    spaceBetween: gallery.margin,
+                    paginationClickable: true,
+                    keyboardControl: true,
+                    centeredSlides: true,
+                    grabCursor: true,
+
+                    preloadImages: false,
+                    lazyLoading: true,
+                    lazyLoadingInPrevNext: true,
+
+                    freeMode: gallery.freeMode,
+                    freeModeSticky: true
+                });
+
+            }
 
         }
 
