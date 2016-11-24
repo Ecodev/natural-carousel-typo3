9692c9492368410e3bf70622f1012af05d90f96e
Samuel Baptista <samuel.baptista@ecodev.ch>
2016-11-24 10:16:56 +0100

    Update natural-carousel-js

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_email' => 'fabien.udriot@ecodev.ch, samuel.baptista@ecodev.ch',
   'author_company' => 'Ecodev',
   'state' => 'beta',
-  'version' => '1.0.2-dev',
+  'version' => '1.0.3-dev',
   'constraints' =>
   array (
     'depends' =>
