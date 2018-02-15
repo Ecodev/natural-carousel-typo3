1a86e3744ad4b3f78b0693a6020f57cd1ce886cd
Fiacre Sankara <fiacre2000@gmail.com>
2018-02-15 02:35:15 +0000

    [FEATURE] Patch for compatibility on TYPO3 CMS8 (#4)

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '7.6.0-7.99.99',
+      'typo3' => '7.6.0-8.99.99',
       'vidi' => '',
     ),
     'conflicts' =>
