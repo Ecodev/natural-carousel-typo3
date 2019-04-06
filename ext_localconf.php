b84631a0e97aef7d412640cf88f132224737d18f
Fabien Udriot <fabien@omic.ch>
2019-04-06 17:21:01 +0200

    Fix BE wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,46 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Fab.natural_carousel',
-    'Pi1',
-    array(
-        'Carousel' => 'list',
-    ),
-    // non-cachable actions
-    array()
+call_user_func(
+    function () {
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'Fab.natural_carousel',
+            'Pi1',
+            [
+                'Carousel' => 'list',
+            ],
+            // non-cachable actions
+            []
+        );
+
+        // Register icons
+        $icons = [
+            'content-natural-carousel' => 'EXT:natural_carousel/ext_icon.png',
+        ];
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        foreach ($icons as $identifier => $path) {
+            $iconRegistry->registerIcon(
+                $identifier, TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class, ['source' => $path]
+            );
+        }
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            'mod {
+                wizards.newContentElement.wizardItems.plugins {
+                    elements {
+                        tx_naturalcarousel_templatebasedcontent {
+                            iconIdentifier = content-natural-carousel
+                            title = LLL:EXT:natural_carousel/Resources/Private/Language/locallang.xlf:wizard.title
+                            description = LLL:EXT:natural_carousel/Resources/Private/Language/locallang.xlf:wizard.description
+                            tt_content_defValues {
+                                CType = list
+                                list_type = naturalcarousel_pi1
+                            }
+                        }
+                    }
+                }
+            }'
+        );
+    }
 );
--- ext_tables.php
+++ ext_tables.php
@@ -15,7 +15,3 @@ $pluginSignature = strtolower($extensionName) . '_pi1';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,recursive,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:natural_carousel/Configuration/FlexForm/NaturalCarousel.xml');
-
-if (TYPO3_MODE === "BE") {
-	$TBE_MODULES_EXT["xMOD_db_new_content_el"]['addElClasses']['tx_naturalcarousel_wizard'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('natural_carousel') . 'Classes/Backend/Wizard.php';
-}
