2f31b87b23fad9c02017eea830b0a865b0f73386
Fabien Udriot <fabien@omic.ch>
2022-06-29 12:01:04 +0200

    [FIX] Base url

--- Classes/ViewHelpers/SlideStackViewHelper.php
+++ Classes/ViewHelpers/SlideStackViewHelper.php
@@ -15,6 +15,7 @@ use Fab\Vidi\Domain\Model\Content;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ProcessedFile;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
@@ -37,14 +38,10 @@ class SlideStackViewHelper extends AbstractViewHelper
             /** @var \TYPO3\CMS\Core\Resource\File $file */
             $file = $slide->getOriginalFile();
 
-            /** @var \TYPO3\CMS\Core\Http\ServerRequest $request */
-            $request = $GLOBALS['TYPO3_REQUEST'];
-            $site = $request->getAttribute('site');
-            $baseUrl = $site->getBase()->getScheme() . '://' . $site->getBase()->getHost() . $site->getBase()->getPath();
-
+            $baseUrl = GeneralUtility::getIndpEnv('TYPO3_SITE_URL');
             $item = [
-                'thumbnail' => $baseUrl . '/' . $this->createProcessedThumbnail($file)->getPublicUrl(),
-                'enlarged' => $baseUrl . '/' . $this->createProcessedEnlarged($file)->getPublicUrl(),
+                'thumbnail' => $baseUrl . $this->createProcessedThumbnail($file)->getPublicUrl(),
+                'enlarged' => $baseUrl . $this->createProcessedEnlarged($file)->getPublicUrl(),
                 'title' => $slide->getProperty('title'),
                 'desc' => $slide->getProperty('description'),
                 'slideLink' => $slide->getProperty('link'),
